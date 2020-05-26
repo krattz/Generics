a5f7e10c3f6c6f957bfc0b398659cf7ce3bc852c
krattz <nthebep@yahoo.com>
2020-05-26 11:24:05 +0200

    adjusted work

--- src/main/java/GenericClass.java
+++ src/main/java/GenericClass.java
@@ -1,16 +1,25 @@
 public class GenericClass<String, Integer> {
-private String s;
-private Integer i;
-    public void setS(String k){
-     this.s = k;
+    private String s;
+    private Integer i;
+
+    public void setS(String k) {
+        this.s = k;
     }
-    public void setI(Integer i){
+    public void setI(Integer i) {
         this.i = i;
     }
-    public void getS(){
-        System.out.println(s);
+    public String getS() {
+       return s;
     }
-    public void getI(){
-        System.out.println(i);
+    public Integer getI() {
+       return i;
+    }
+    public void printArrayContent() {
+        if (getI() != null){
+            System.out.println(getI());
+        }
+       if (getS() != null) {
+           System.out.println(getS());
+       }
     }
 }
--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -2,12 +2,10 @@ public class MainProgram {
     public static void main(String[] args) {
        GenericClass stringInstance = new GenericClass<String, Integer>();
         stringInstance.setS("Test");
-        stringInstance.getS();
+        stringInstance.printArrayContent();
 
         GenericClass integerInstance = new GenericClass<String, Integer>();
         integerInstance.setI(1000);
-        integerInstance.getI();
-
-
+        integerInstance.printArrayContent();
     }
 }
