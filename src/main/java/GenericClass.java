4a5c5e7199426c9647190c01e6562a9d13ca44b9
krattz <nthebep@yahoo.com>
2020-06-01 13:40:33 +0200

    Changes made as per reccomendations

--- src/main/java/GenericClass.java
+++ src/main/java/GenericClass.java
@@ -4,10 +4,7 @@ public class GenericClass<String, Integer> {
     private String s;
     private Integer i;
 
-
-    public void setS(String k) {
-        this.s = k;
-    }
+    public void setS(String s){this.s = s; }
     public void setI(Integer i) {
         this.i = i;
     }
@@ -17,12 +14,18 @@ public class GenericClass<String, Integer> {
     public Integer getI() {
        return i;
     }
-public GenericClass <String,Integer> printArrayContent(){
-    if (getI() != null){
-        System.out.println(getI());
-    }
-    if (getS() != null) {
-        System.out.println(getS());
+public GenericClass <String,Integer> printArrayContent(Integer[] a, String[]b){
+        for(int p = 0; p<a.length; p++) {
+            setI(a[p]);
+            if (getI() != null) {
+                    System.out.println(getI());
+            }
+        }
+    for(int q = 0; q<b.length; q++) {
+        setS(b[q]);
+        if (getS() != null) {
+                System.out.println(getS());
+        }
     }
     return null;
 }
--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,11 +1,8 @@
 public class MainProgram {
     public static void main(String[] args) {
        GenericClass stringInstance = new GenericClass<String, Integer>();
-        stringInstance.setS("Test");
-        stringInstance.printArrayContent();
-
-        GenericClass integerInstance = new GenericClass<String, Integer>();
-        integerInstance.setI(1000);
-        integerInstance.printArrayContent();
+        Integer[] m = {1,2,3};
+        String []n = {"This", "should","be","black"};
+        stringInstance.printArrayContent(m,n);
     }
 }
